f569ccb3b2a3eeb7ad76704d8ee0ea19b959fad3
prateek-srivastava001 <prateeksrivastava201@gmail.com>
2023-06-08 13:43:36 +0530

    added routes

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('./models/User');
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
 const port = 3000;
-const os = require('os');
 const fs = require('fs');
+
+mongoose.connect('mongodb://127.0.0.1:27017/AuthARity', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB', err);
+  });
+
+  // app.set('view engine', 'ejs');
+  // app.set('views', path.join(__dirname, 'views'));
+  
 
 let collectedKeystrokes = [];
 let latitude, longitude, mean, variance;
-
+app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "views")));
@@ -94,3 +112,56 @@ async function ipAddr() {
     console.error("Error:", error);
   }
 }
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    let user = await User.findOne({ username });
+    if (!user) {
+      console.error('User not found', err);
+      res.status(401).json({ error: 'User not found' });
+      return;
+    }
+    if (user && password === user.password) {
+      const payload = {
+        userId: user._id
+      };
+      const token = jwt.sign(payload, 'abcdefgh');
+      res.cookie('token', token, { httpOnly: true });
+      return res.redirect('/dashboard');
+    } 
+    else 
+    {
+      return res.status(200).json({ error: 'Invalid Password' });
+    }
+  } catch (err) {
+    console.error('Error authenticating user', err);
+    return res.status(500).send('Internal Server Error');
+  }
+});
+
+app.post('/register', async (req, res) => {
+  const { name, username, email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Username or email already exists' });
+    }
+
+    const newUser = new User({
+      name,
+      username,
+      email,
+      password
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Error registering user' });
+  }
+});
